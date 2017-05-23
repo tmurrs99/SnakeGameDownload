ac26046e4a09fa16550b8610d6c4afe009b00dfc
tmurrs99 <tyler.murray0@gmail.com>
2017-05-22 21:20:22 -0400

    Delete Locatable.java

--- Locatable.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface Locatable
-{
-	//all objects implementing Locatable have a location that can be accessed
-	public ArrayList<Location> location();
-	
-	public void move();
-}
