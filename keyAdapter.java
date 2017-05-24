fd0a8fef9ebcf2ee616964cee5efa8aeeeb37705
yungneech <niqra99@gmail.com>
2017-05-24 16:35:51 -0400

    Update keyAdapter.java

--- keyAdapter.java
+++ keyAdapter.java
@@ -18,26 +18,26 @@ private class TAdapter extends KeyAdapter {
 
             int key = e.getKeyCode();
 
-            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
-                leftDirection = true;
-                upDirection = false;
-                downDirection = false;
+            if ((key == KeyEvent.VK_LEFT) && (!Direction.EAST)) {
+                Direction.WEST = true;
+                Direction.NORTH = false;
+                Direction.SOUTH = false;
             }
 
-            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
-                rightDirection = true;
-                upDirection = false;
-                downDirection = false;
+            if ((key == KeyEvent.VK_RIGHT) && (!Direction.WEST)) {
+                Direction.EAST = true;
+                Direction.NORTH = false;
+                Direction.SOUTH = false;
             }
 
-            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
-                upDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+            if ((key == KeyEvent.VK_UP) && (!Direction.SOUTH)) {
+                Direction.NORTH = true;
+                Direction.EAST = false;
+                Direction.WEST = false;
             }
 
-            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
-                downDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+            if ((key == KeyEvent.VK_DOWN) && (!Direction.NORTH)) {
+                Direction.SOUTH = true;
+                Direction.EAST = false;
+                Direction.WEST = false;
             }
