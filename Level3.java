59404d0350994f2cc5bfb00a17c6171e9d5bcb24
jesset2 <jesseplatinum91@gmail.com>
2017-06-06 23:17:56 -0400

    Create Level3.java

---
+++ Level3.java
@@ -0,0 +1,30 @@
+Level level3 = new Level(theEnv, true, ent2, 5, score); 
+Player = new Snake(theEnv, new Location(22,7);
+ent3.add(new Food(theEnv, new Location(18, 9);
+theEnv.add(new Obstacles(theEnv, new Location (6,2)));
+theEnv.add(new Obstacles(theEnv, new Location (6,12)));
+theEnv.add(new Obstacles(theEnv, new Location (6,20)));
+theEnv.add(new Obstacles(theEnv, new Location (7,3)));
+theEnv.add(new Obstacles(theEnv, new Location (7,13)));
+theEnv.add(new Obstacles(theEnv, new Location (7,21)));
+theEnv.add(new Obstacles(theEnv, new Location (8,4)));
+theEnv.add(new Obstacles(theEnv, new Location (8,14)));
+theEnv.add(new Obstacles(theEnv, new Location (8,22)));
+theEnv.add(new Obstacles(theEnv, new Location (9,5)));
+theEnv.add(new Obstacles(theEnv, new Location (9,15)));
+theEnv.add(new Obstacles(theEnv, new Location (9,23)));
+theEnv.add(new Obstacles(theEnv, new Location (16,2)));
+theEnv.add(new Obstacles(theEnv, new Location (16,12)));
+theEnv.add(new Obstacles(theEnv, new Location (16,20)));
+theEnv.add(new Obstacles(theEnv, new Location (17,3)));
+theEnv.add(new Obstacles(theEnv, new Location (17,13)));
+theEnv.add(new Obstacles(theEnv, new Location (17,21)));
+theEnv.add(new Obstacles(theEnv, new Location (18,4)));
+theEnv.add(new Obstacles(theEnv, new Location (18,14)));
+theEnv.add(new Obstacles(theEnv, new Location (18,22)));
+theEnv.add(new Obstacles(theEnv, new Location (19,5)));
+theEnv.add(new Obstacles(theEnv, new Location (19,15)));
+theEnv.add(new Obstacles(theEnv, new Location (19,23)));
+ 
+ 
+theEnv.add(new Turret(theEnv, new Location(13,23), Direction.WEST, 4));
